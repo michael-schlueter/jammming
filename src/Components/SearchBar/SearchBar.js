8316215b4abb35320280f09b6e3d2aa1cd248e35
Raulinhotic <schluet0r4life@gmail,com>
2021-04-03 14:52:31 +0200

    App with basic functionality

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./SearchBar.css";
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: "",
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  // Event Handler for changing the search term
+  handleTermChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input
+          placeholder="Enter A Song Title"
+          onChange={this.handleTermChange}
+        />
+        {/* Needs to be added */ }
+        <button className="SearchButton" onClick={this.search}>
+          SEARCH
+        </button>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
---
+++ src/Components/Track/Track.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Track.css";
+
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  // Event Handler for adding a track
+  addTrack(event) {
+    this.props.onAdd(this.props.track);
+  }
+
+  // Event Handler for removing a track
+  removeTrack(event) {
+    this.props.onRemove(this.props.track);
+  }
+
+  // Rendering a button depending on if a track is removable or addable
+  renderAction() {
+    if (this.props.isRemoval) {
+      return (
+        <button className="Track-action" onClick={this.removeTrack}>
+          -
+        </button>
+      );
+    }
+    return (
+      <button className="Track-action" onClick={this.addTrack}>
+        +
+      </button>
+    );
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  }
+}
+
+export default Track;
